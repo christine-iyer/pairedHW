56c98aa1230235585d2a948faa5121ca88c26176
chrisIyer <hellohotbox420@gmail.com>
2023-01-21 21:06:40 -0500

    Co-authored-by: Rosa Pena <rpena124@users.noreply.github.com>

--- Challenge5/Challenge5.js
+++ Challenge5/Challenge5.js
@@ -1,6 +1,6 @@
-const mockBlog = require('./Challenge5/seed')
+const mockBlog = require('./seed')
 const express = require('express')
-const Blog =require('./Challenge5/blogSchema')
+const Blog =require('./blogSchema')
 
 
 const app = express()
@@ -12,14 +12,14 @@ app.get('/blog', (req,res) => {
 //show
 app.get('/blog/:id', (req, res) => {
      res.send(mockBlog[req.params.id]);
- })
+})
 
  //create
  app.post('/blog', (req,res) => {
+     res.send(req.body)
  }
- 
+
  )
 app.listen(3008,() => {
      console.log("I hear you on this port 3008")
 })
-
